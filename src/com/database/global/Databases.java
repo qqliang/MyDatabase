1a36bac4c550b9b21b78dafc62efb23d326e70b9
zhangjie16 <550048625@qq.com>
2016-11-29 22:27:42 +0800

    Create Databases.java
    加了个Databases.java,用于定义一个全局使用的一个数据库，定义了数据库名和数据库打开状态（1，打开  2，关闭）。

---
+++ src/com/database/global/Databases.java
@@ -0,0 +1,19 @@
+package com.database.global;
+
+public class Databases {
+	
+	public static String name;
+	public static int stat;
+	public String getName() {
+		return name;
+	}
+	
+	public int getStat() {
+		return stat;
+	}
+	public void setStat(int stat) {
+		this.stat = stat;
+	}
+	
+
+}
