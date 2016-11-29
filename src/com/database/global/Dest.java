9b109707098ac4ad82344ebda35290947760e424
zhangjie16 <550048625@qq.com>
2016-11-29 22:23:10 +0800

    Update Dest.java
    加了几个关键字状态

--- src/com/database/global/Dest.java
+++ src/com/database/global/Dest.java
@@ -6,8 +6,15 @@ package com.database.global;
  *
  */
 public class Dest {
-	public final static short TABLE = 10;
-	public final static short DATABASE = 11;
-	public final static short INDEX = 12;
+	public final static boolean SUCCESS=true;
+	public final static boolean ERROR=false;
+	public final static short OPENED=1;     //数据库打开状态
+	public final static short CLOSED=2;     //数据库关闭状态
+	public final static short TABLE = 10;   //创建表返回关键字状态码
+	public final static short DATABASE = 11; //创建数据库返回关键字状态码
+	public final static short INDEX = 12;    
 	public final static short VIEW = 13;
+	public final static short SELECT=21;     //select语句关键字状态码
+	public final static short INSERT=22;     //insert语句关键字状态码
+	public final static short DELETE=23;     //delete语句关键字状态码
 }
