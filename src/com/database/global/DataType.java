2a795b3cab55170ad8f34e45a1ae31b3ea227b61
MimeZoe0628 <xiaoxue3348486@163.com>
2016-12-02 22:33:50 +0800

    添加空间分配配置类、定义数据类型

---
+++ src/com/database/global/DataType.java
@@ -0,0 +1,10 @@
+package com.database.global;
+
+/**
+ * Created by zoe on 2016/12/2.
+ */
+public class DataType {
+    public static byte INTEGER = 1;
+    public static byte LONG = 2;
+    public static byte TEXT = 3;
+}
---
+++ src/com/database/global/SpaceAllocation.java
@@ -0,0 +1,9 @@
+package com.database.global;
+
+/**
+ * Created by zoe on 2016/12/2.
+ */
+public class SpaceAllocation {
+    public static int PAGE_SIZE = 1024;
+    public static int SECTOR_SIZE = 512;
+}
