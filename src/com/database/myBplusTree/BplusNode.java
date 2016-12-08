b4ae5f10525ab96ecb362a7dc58fc3a6df9a100f
Qing_L <qqliang@uniplore.io>
2016-12-08 17:26:19 +0800

    修改B树

--- src/com/database/myBplusTree/BplusNode.java
+++ src/com/database/myBplusTree/BplusNode.java
@@ -3,51 +3,100 @@ package com.database.myBplusTree;
 /**
  * Created by Qing_L on 2016/11/23.
  */
+import com.database.global.ColumnConstraint;
+import com.database.global.DataType;
+import com.database.pager.Column;
+import com.database.pager.Page;
+import com.database.pager.Pager;
+import com.database.pager.Record;
+
 import java.util.*;
 import java.util.Map.Entry;
 import java.util.AbstractMap.SimpleEntry;
 
 public class BplusNode {
 
-    /** 是否为叶子节点 */
-    protected boolean isLeaf;
+    Pager pager ;
+    Page page;
 
-    /** 是否为根节点*/
-    protected boolean isRoot;
+    private int flags;                //节点类型：0：根节点；1：内部节点；2：叶子节点；
+    private BplusNode parent;         //父节点
+    private BplusNode previous;       //叶子节点的前一个节点
+    private BplusNode next;           //叶子节点的下一个节点
+    private List<BplusNode> children; //孩子节点
 
-    /** 父节点 */
-    protected BplusNode parent;
+    //记录信息：rowid->data。若为内部节点，data为孩子节点的页号，若为叶子节点，data为一条记录
+    private List<Entry<Integer, String>> entries;
 
-    /** 叶节点的前节点*/
-    protected BplusNode previous;
+    public int getFlags() {
+        return flags;
+    }
+    public void setFlags(int flags) {
+        this.flags = flags;
+    }
 
-    /** 叶节点的后节点*/
-    protected BplusNode next;
+    public BplusNode getParent() {
+        return parent;
+    }
+    public void setParent(BplusNode parent) {
+        this.parent = parent;
+    }
 
-    /** 节点的关键字 */
-    protected List<Entry<Integer, String>> entries;
+    public BplusNode getPrevious() {
+        return previous;
+    }
+    public void setPrevious(BplusNode previous) {
+        this.previous = previous;
+    }
 
-    /** 子节点 */
-    protected List<BplusNode> children;
+    public BplusNode getNext() {
+        return next;
+    }
+    public void setNext(BplusNode next) {
+        this.next = next;
+    }
 
-    public BplusNode(boolean isLeaf) {
-        this.isLeaf = isLeaf;
+    public List<BplusNode> getChildren() {
+        return children;
+    }
+    public void setChildren(List<BplusNode> children) {
+        this.children = children;
+    }
+
+    public List<Entry<Integer, String>> getEntries() {
+        return entries;
+    }
+    public void setEntries(List<Entry<Integer, String>> entries) {
+        this.entries = entries;
+    }
+
+    /**
+     * 构造函数
+     */
+    public BplusNode(int type) {
+        this.flags = type;
         entries = new ArrayList<Entry<Integer, String>>();
 
-        if (!isLeaf) {
+        //如果不是叶子节点，则添加孩子指针
+        if (type != 2) {
             children = new ArrayList<BplusNode>();
         }
+
+        //新建页面
+//        page = pager.getPage();
     }
 
-    public BplusNode(boolean isLeaf, boolean isRoot) {
-        this(isLeaf);
-        this.isRoot = isRoot;
+    public BplusNode(Pager pager,int type) {
+        this(type);
+        this.pager = pager;
     }
 
+    /**
+     * 根据关键字，查找数据（采用二分查找）
+     */
     public String get(Integer key) {
-
         //如果是叶子节点
-        if (isLeaf) {
+        if (flags == 2) {
             int low = 0, high = entries.size() - 1, mid;
             int comp ;
             while (low <= high) {
@@ -64,14 +113,16 @@ public class BplusNode {
             //未找到所要查询的对象
             return null;
         }
-        //如果不是叶子节点
-        //如果key小于节点最左边的key，沿第一个子节点继续搜索
+        /**
+         * 如果不是叶子节点
+         * 1、如果key小于节点最左边的key，沿第一个子节点继续搜索
+         * 2、如果key大于等于节点最右边的key，沿最后一个子节点继续搜索
+         * 3、否则沿比key大的前一个子节点继续搜索
+         */
         if (key.compareTo(entries.get(0).getKey()) < 0) {
             return children.get(0).get(key);
-            //如果key大于等于节点最右边的key，沿最后一个子节点继续搜索
         }else if (key.compareTo(entries.get(entries.size()-1).getKey()) >= 0) {
             return children.get(children.size()-1).get(key);
-            //否则沿比key大的前一个子节点继续搜索
         }else {
             int low = 0, high = entries.size() - 1, mid= 0;
             int comp ;
@@ -91,9 +142,15 @@ public class BplusNode {
     }
 
     public void insertOrUpdate(Integer key, String value, BplusTree tree){
-        //如果是叶子节点
-        if (isLeaf){
-            //不需要分裂，直接插入或更新
+        /**
+         * 如果是叶子节点
+         */
+        if (flags == 2){
+            /**
+             * 不需要分裂的情况：
+             * 1、节点中包含该关键字，则直接修改数据域
+             * 2、节点中不包含该关键字，而该节点未满，则插入数据
+             */
             if (contains(key) != -1 || entries.size() < tree.getOrder()){
                 insertOrUpdate(key, value);
                 if(tree.getHeight() == 0){
@@ -101,11 +158,24 @@ public class BplusNode {
                 }
                 return ;
             }
-            //需要分裂
+            /**
+             * 需要分裂的情况：节点不包含该关键字，且该节点已满，则分裂节点
+             */
             //分裂成左右两个节点
-            BplusNode left = new BplusNode(true);
-            BplusNode right = new BplusNode(true);
-            //设置链接
+            BplusNode left = new BplusNode(pager,1);
+            BplusNode right = new BplusNode(pager,1);
+            /**
+             * 设置链接
+             * 1、创建两个新的节点：left、right；
+             * 2、若前一个节点不为空：
+             *      1)把前一个节点的next指针指向left
+             *      2)把left的previous指针指向前一个节点
+             * 3、若后一个节点不为空：
+             *      1)把后一个节点的previous指针指向right
+             *      2)把right的next指针指向后一个节点
+             * 4、left的next指针指向right，right的previous指针指向left
+             * 5、把当前节点的previous、next指针赋值为NULL
+             */
             if (previous != null){
                 previous.next = left;
                 left.previous = previous ;
@@ -117,16 +187,35 @@ public class BplusNode {
             if (previous == null){
                 tree.setHead(left);
             }
-
             left.next = right;
             right.previous = left;
             previous = null;
             next = null;
 
-            //复制原节点关键字到分裂出来的新节点
+            /**
+             * 将当前节点的entry分给left和right节点
+             */
             copy2Nodes(key, value, left, right, tree);
 
-            //如果不是根节点
+            /**
+             * 调整当前节点的父子关系指针
+             * 若当前节点不是根节点
+             *      1)将父节点指向当前节点的children指针移除
+             *      2)left的parent指针指向父节点
+             *      3)right的parent指针指向父节点
+             *      4)父节点的children指针添加left和right
+             *      5)父节点的entries添加right的第0条记录。（left的第0条记录，父节点中已有）
+             *      6)删除当前节点的entries记录和children指针
+             *      7)更新父节点的关键字
+             *      8)删除当前节点的parent指针
+             * 若当前节点是根节点
+             *      1)新建一个parent节点作为根节点
+             *      2)树的根节点设置为parent节点
+             *      3)left和right节点的parent指针指向parent节点
+             *      4)parent节点的子节点指针添加left和right节点
+             *      5)parent节点的entries记录赋值为当前节点的entries
+             *      6)清空当前节点的指针和记录
+             */
             if (parent != null) {
                 //调整父子节点关系
                 int index = parent.children.indexOf(this);
@@ -144,8 +233,8 @@ public class BplusNode {
                 parent = null; //删除当前节点的父节点引用
                 //如果是根节点
             }else {
-                isRoot = false;
-                BplusNode parent = new BplusNode (false, true);
+                flags = 1;
+                BplusNode parent = new BplusNode (pager, 0);
                 tree.setRoot(parent);
                 left.parent = parent;
                 right.parent = parent;
@@ -156,16 +245,17 @@ public class BplusNode {
                 children = null;
             }
             return ;
-
         }
-        //如果不是叶子节点
-        //如果key小于等于节点最左边的key，沿第一个子节点继续搜索
+        /**
+         * 如果不是叶子节点
+         * 1、如果key小于等于节点最左边的key，沿第一个子节点继续搜索
+         * 2、如果key大于节点最右边的key，沿最后一个子节点继续搜索
+         * 3、否则沿比key大的前一个子节点继续搜索
+         */
         if (key.compareTo(entries.get(0).getKey()) < 0) {
             children.get(0).insertOrUpdate(key, value, tree);
-            //如果key大于节点最右边的key，沿最后一个子节点继续搜索
         }else if (key.compareTo(entries.get(entries.size()-1).getKey()) >= 0) {
             children.get(children.size()-1).insertOrUpdate(key, value, tree);
-            //否则沿比key大的前一个子节点继续搜索
         }else {
             int low = 0, high = entries.size() - 1, mid= 0;
             int comp ;
@@ -187,8 +277,8 @@ public class BplusNode {
         }
     }
 
-    private void copy2Nodes(Integer key, String value, BplusNode left,
-                            BplusNode right,BplusTree tree) {
+    /** 将当前节点的entry记录分给left和right节点 */
+    private void copy2Nodes(Integer key, String value, BplusNode left, BplusNode right,BplusTree tree) {
         //左右两个节点关键字长度
         int leftSize = (tree.getOrder() + 1) / 2 + (tree.getOrder() + 1) % 2;
         boolean b = false;//用于记录新元素是否已经被插入
@@ -220,11 +310,31 @@ public class BplusNode {
     /** 插入节点后中间节点的更新 */
     protected void updateInsert(BplusTree tree){
 
-        //如果子节点数超出阶数，则需要分裂该节点
+        /**
+         * 如果子节点数超出阶数，则需要分裂该节点
+         *      1)分裂成左右两个节点：left、right
+         *      2)计算left和right节点的子节点的长度
+         *      3)将当前节点的子节点赋值给left和right，并把子节点的parent指针指向left和right
+         *      4)把当前节点的entries记录赋值给left和right的entries记录
+         *如果不是根节点
+         *      1)将当前节点从父节点的children指针中移除
+         *      2)left和right节点的parent指针指向父节点
+         *      3)父节点的children指针添加left和right节点
+         *      4)父节点的entries记录添加right节点的第0条entries记录
+         *      6)更新父节点的记录和指针
+         *      7)清空当前节点的指针和记录
+         *如果是根节点
+         *      1)新建一个parent节点作为根节点
+         *      2)设置树的根节点为parent节点，并将树高+1
+         *      3)left和right节点的parent指针指向parent节点
+         *      4)parent节点的子节点指针添加left和right节点
+         *      5)parent节点的entries记录赋值为当前节点的entries
+         *      6)清空当前节点的指针和记录
+         */
         if (children.size() > tree.getOrder()) {
             //分裂成左右两个节点
-            BplusNode left = new BplusNode(false);
-            BplusNode right = new BplusNode(false);
+            BplusNode left = new BplusNode(pager,1);
+            BplusNode right = new BplusNode(pager,1);
             //左右两个节点子节点的长度
             int leftSize = (tree.getOrder() + 1) / 2 + (tree.getOrder() + 1) % 2;
             int rightSize = (tree.getOrder() + 1) / 2;
@@ -262,8 +372,8 @@ public class BplusNode {
                 parent = null;
                 //如果是根节点
             }else {
-                isRoot = false;
-                BplusNode parent = new BplusNode(false, true);
+                flags = 1;
+                BplusNode parent = new BplusNode(pager, 0);
                 tree.setRoot(parent);
                 tree.setHeight(tree.getHeight() + 1);
                 left.parent = parent;
@@ -282,7 +392,7 @@ public class BplusNode {
 
         // 如果子节点数小于M / 2或者小于2，则需要合并节点
         if (children.size() < tree.getOrder() / 2 || children.size() < 2) {
-            if (isRoot) {
+            if (flags == 0) {
                 // 如果是根节点并且子节点数大于等于2，OK
                 if (children.size() >= 2) return;
                 // 否则与子节点合并
@@ -290,7 +400,7 @@ public class BplusNode {
                 tree.setRoot(root);
                 tree.setHeight(tree.getHeight() - 1);
                 root.parent = null;
-                root.isRoot = true;
+                root.flags = 0;
                 entries = null;
                 children = null;
                 return ;
@@ -363,10 +473,9 @@ public class BplusNode {
                 previous.children = null;
                 previous.entries = null;
                 parent.entries.remove(parent.children.indexOf(this));
-                if((!parent.isRoot
-                        && (parent.children.size() >= tree.getOrder() / 2
+                if((!(parent.flags==0) && (parent.children.size() >= tree.getOrder() / 2
                         && parent.children.size() >= 2))
-                        ||parent.isRoot && parent.children.size() >= 2){
+                        ||(parent.flags==0) && parent.children.size() >= 2){
                     return ;
                 }
                 parent.updateRemove(tree);
@@ -392,9 +501,9 @@ public class BplusNode {
                 next.children = null;
                 next.entries = null;
                 parent.entries.remove(parent.children.indexOf(this));
-                if((!parent.isRoot && (parent.children.size() >= tree.getOrder() / 2
+                if((!(parent.flags==0) && (parent.children.size() >= tree.getOrder() / 2
                         && parent.children.size() >= 2))
-                        ||parent.isRoot && parent.children.size() >= 2){
+                        ||(parent.flags==0) && parent.children.size() >= 2){
                     return ;
                 }
                 parent.updateRemove(tree);
@@ -405,13 +514,13 @@ public class BplusNode {
 
     public String remove(Integer key, BplusTree tree){
         //如果是叶子节点
-        if (isLeaf){
+        if (flags == 2){
             //如果不包含该关键字，则直接返回
             if (contains(key) == -1){
                 return null;
             }
             //如果既是叶子节点又是根节点，直接删除
-            if (isRoot) {
+            if (flags == 0) {
                 if(entries.size() == 1){
                     tree.setHeight(0);
                 }
@@ -471,9 +580,9 @@ public class BplusNode {
                     previous = null;
                 }
                 parent.entries.remove(parent.children.indexOf(this));
-                if((!parent.isRoot && (parent.children.size() >= tree.getOrder() / 2
+                if((!(parent.flags==0) && (parent.children.size() >= tree.getOrder() / 2
                         && parent.children.size() >= 2))
-                        ||parent.isRoot && parent.children.size() >= 2){
+                        ||(parent.flags==0) && parent.children.size() >= 2){
                     return returnValue;
                 }
                 parent.updateRemove(tree);
@@ -505,9 +614,9 @@ public class BplusNode {
                 }
                 //更新父节点的关键字列表
                 parent.entries.remove(parent.children.indexOf(this));
-                if((!parent.isRoot && (parent.children.size() >= tree.getOrder() / 2
+                if((!(parent.flags==0) && (parent.children.size() >= tree.getOrder() / 2
                         && parent.children.size() >= 2))
-                        ||parent.isRoot && parent.children.size() >= 2){
+                        ||(parent.flags==0) && parent.children.size() >= 2){
                     return returnValue;
                 }
                 parent.updateRemove(tree);
@@ -541,7 +650,10 @@ public class BplusNode {
         }
     }
 
-    /** 判断当前节点是否包含该关键字*/
+    /**
+     * 判断当前节点是否包含该关键字
+     * 若包含该关键字，则返回-1，若包含该关键字，则返回entries中的位置
+     */
     protected int contains(Integer key) {
         int low = 0, high = entries.size() - 1, mid;
         int comp ;
@@ -600,11 +712,8 @@ public class BplusNode {
     }
     public String toString(){
         StringBuilder sb = new StringBuilder();
-        sb.append("isRoot: ");
-        sb.append(isRoot);
-        sb.append(", ");
-        sb.append("isLeaf: ");
-        sb.append(isLeaf);
+        sb.append("flags: ");
+        sb.append(flags);
         sb.append(", ");
         sb.append("keys: ");
         for (Entry<Integer,String> entry : entries){
@@ -615,5 +724,28 @@ public class BplusNode {
         return sb.toString();
 
     }
+    public Record getRecord() {
+        Record record = new Record();
+        List<Column> cols = new ArrayList<Column>();
+        Column idCol =  new Column();
+        idCol.setName("id");
+        idCol.setType(DataType.INTEGER);
+        idCol.setConstraint(ColumnConstraint.NONE);
 
+        Column nameCol =  new Column();
+        nameCol.setName("name");
+        nameCol.setType(DataType.TEXT);
+        nameCol.setConstraint(ColumnConstraint.NONE);
+
+        Column ageCol =  new Column();
+        ageCol.setName("age");
+        ageCol.setType(DataType.TINY_INT);
+        ageCol.setConstraint(ColumnConstraint.NONE);
+
+        cols.add(idCol);
+        cols.add(nameCol);
+        cols.add(ageCol);
+        record.setColumns(cols);
+        return record;
+    }
 }
