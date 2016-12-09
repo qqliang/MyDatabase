ddd682da92f2043a10003a502f71282a7cd6ca0b
Qing_L <qqliang@uniplore.io>
2016-12-09 13:37:43 +0800

    增加页面类型

--- src/com/database/global/PageType.java
+++ src/com/database/global/PageType.java
@@ -6,4 +6,6 @@ package com.database.global;
 public class PageType {
     public static final byte TABLE_LEAF = 0x0d;
     public static final byte TABLE_INTERNAL = 0x05;
+    public static final byte TABLE_ROOT = 0x01;
+    public static final byte TABLE_OVERFLOW = 0x02;
 }
