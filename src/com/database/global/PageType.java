95a38c61f9f6acc907ade2c6b70b0e6bde261344
MimeZoe0628 <xiaoxue3348486@163.com>
2016-12-09 13:25:53 +0800

    添加页面类型定义

---
+++ src/com/database/global/PageType.java
@@ -0,0 +1,9 @@
+package com.database.global;
+
+/**
+ * Created by zoe on 2016/12/8.
+ */
+public class PageType {
+    public static final byte TABLE_LEAF = 0x0d;
+    public static final byte TABLE_INTERNAL = 0x05;
+}
