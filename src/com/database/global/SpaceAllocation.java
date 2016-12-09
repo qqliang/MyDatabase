ba003b01ff8d8f7c9552bcd6ae4d30d1f27d603d
MimeZoe0628 <xiaoxue3348486@163.com>
2016-12-09 13:26:40 +0800

    添加类SpaceAllocation

--- src/com/database/global/SpaceAllocation.java
+++ src/com/database/global/SpaceAllocation.java
@@ -6,7 +6,7 @@ package com.database.global;
 public class SpaceAllocation {
     public static int PAGE_SIZE = 1024;
     public static int SECTOR_SIZE = 512;
-    public static int PAGE_RESERVED = 8;
+    public static int PAGE_RESERVED = 0;
     public static int PAGE_HEADER_SIZE = 50;
-    public static int RECORD_HEADER = 4;
+    public static int RECORD_HEADER = 4+4;      //rowid，hdrsz
 }
