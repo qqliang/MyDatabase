3a8b4608ff5e16890cbbcfd76705550d8c73ec81
MimeZoe0628 <xiaoxue3348486@163.com>
2016-12-09 15:41:25 +0800

    修改测试类

--- src/com/database/main/TestPager.java
+++ src/com/database/main/TestPager.java
@@ -16,29 +16,48 @@ import java.util.Map;
  * Created by zoe on 2016/12/5.
  */
 public class TestPager {
+    private static Database database ;
+    private static TableSchema schema;
+    private static Pager pager ;
+
+    /**
+     * 对类对象进行初始化
+     */
+    public  static void init(){
+        database = new Database();
+        database.openDB("test2");
+        pager  = new Pager(database);
+        schema = getSchema();
+    }
+
     public static void main(String[] args){
+        init();
 //        testRead();
 //        testReadByRowid();
 //        testFlush();
         testLoad();
     }
+
+    /**
+     *  测试数据刷新到磁盘
+     */
     public static void testFlush(){
-        Database database = new Database();
-        database.openDB("test2");
-        Pager pager = new Pager(database);
-        TableSchema record = getRecord();
+
         int rowid = 0;
-        pager.writeData(1, record.getBytes(++rowid, "1,zhouyu,22"));
-        pager.writeData(1, record.getBytes(++rowid,"2,lqq,22"));
-        pager.writeData(1, record.getBytes(++rowid,"3,hh,26"));
+        pager.writeData(1, schema.getBytes(++rowid, "1,zhouyu,22"));
+        pager.writeData(1, schema.getBytes(++rowid,"2,lqq,22"));
+        pager.writeData(1, schema.getBytes(++rowid,"3,hh,26"));
 
-        pager.writeData(1, record.getBytes(++rowid,"4,dxr,27"));
-        pager.writeData(1, record.getBytes(++rowid,"5,yyc,27"));
-        pager.writeData(1, record.getBytes(++rowid,"6,whw,22"));
-
+        pager.writeData(1, schema.getBytes(++rowid,"4,dxr,27"));
+        pager.writeData(1, schema.getBytes(++rowid,"5,yyc,27"));
+        pager.writeData(1, schema.getBytes(++rowid,"6,whw,22"));
         pager.flush();
 
     }
+
+    /**
+     * 测试数据是否能从数据库文件正确的加载
+     */
     public static void testLoad()
     {
         Database database = new Database();
@@ -49,34 +68,41 @@ public class TestPager {
         List<Map.Entry<Integer, String>> list = pager.readRecord(1);
         System.out.println(list);
     }
+
+    /**
+     * 测试是否能读取页面数据
+     */
     public static void testRead(){
-        Database database = new Database();
-        database.openDB("test");
-        Pager pager = new Pager(database);
-        TableSchema record = getRecord();
+
         int rowid = 0;
-        pager.writeData(1,record.getBytes(rowid++,"1,zhouyu,22"));
-        pager.writeData(1,record.getBytes(rowid++,"2,lqq,22"));
-        pager.writeData(1,record.getBytes(rowid++,"3,hh,26"));
+        pager.writeData(1,schema.getBytes(rowid++,"1,zhouyu,22"));
+        pager.writeData(1,schema.getBytes(rowid++,"2,lqq,22"));
+        pager.writeData(1,schema.getBytes(rowid++,"3,hh,26"));
 
         List<Map.Entry<Integer, String>> list = pager.readRecord(1);
         System.out.println(list.toString());
     }
+
+    /**
+     * 测试通过rowid对某一条目的查找
+     */
     public static void testReadByRowid(){
-        Database database = new Database();
-        database.openDB("test");
-        Pager pager = new Pager(database);
-        TableSchema record = getRecord();
+
         pager.getPages()[1].setPageType(PageType.TABLE_LEAF);
         int rowid = 0;
-        pager.writeData(1,record.getBytes(rowid++,"1,zhouyu,22"));
-        pager.writeData(1,record.getBytes(rowid++,"2,lqq,22"));
-        pager.writeData(1,record.getBytes(rowid++,"3,hh,26"));
+        pager.writeData(1,schema.getBytes(rowid++,"1,zhouyu,22"));
+        pager.writeData(1,schema.getBytes(rowid++,"2,lqq,22"));
+        pager.writeData(1,schema.getBytes(rowid++,"3,hh,26"));
 
         String row = pager.readDataByRowid(1,2).getValue();
         System.out.println("row:"+row);
     }
-    public static TableSchema getRecord() {
+
+    /**
+     *  自己构造的模式对象
+     * @return
+     */
+    public static TableSchema getSchema() {
         TableSchema record = new TableSchema();
         List<Column> cols = new ArrayList<Column>();
         Column idCol =  new Column();
