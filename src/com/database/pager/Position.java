2d54e9446102213ca1df2c3ce4093d680a31ec62
MimeZoe0628 <xiaoxue3348486@163.com>
2016-12-12 15:31:00 +0800

    修改Position

--- src/com/database/pager/Position.java
+++ src/com/database/pager/Position.java
@@ -4,8 +4,6 @@ package com.database.pager;
  * Created by zoe on 2016/12/8.
  */
 public class Position {
-    public static int MAXROWID_IN_ROOT = 120;
-
     public static int PGNO_IN_PAGE = 0 ;
     public static byte PGTYPE_IN_PAGE = 4;
     public static int OFFSET_IN_PAGE = 5;
@@ -13,9 +11,16 @@ public class Position {
     public static int PARENT_PAGE_IN_PAGE = 13;
     public static int PREV_PAGE_IN_PAGE = 17;
     public static int NEXT_PAGE_IN_PAGE = 21;
-    public static byte CELLNUM_IN_PAGE = 25;
-    public static int CELL_IN_PAGE = 26;
+    public static byte ORDER_IN_BPLUS_ROOT = 25;
+    public static int HEAD_IN_BPLUS_ROOT = 26;
+    public static int MAX_ROWID_IN_BPLIS_ROOT = 30;
+    public static byte CELLNUM_IN_PAGE = 34;
+    public static int CELL_IN_PAGE = 35;
 
     public static int ROWID_IN_RECORD = 0;
     public static int HEADER_IN_RECORD = 4;
+
+
+    //page 1 header
+    public static int TABLE_COUNT_IN_FIRST_PAGE = 120;
 }
